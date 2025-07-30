a116b9a815712365da7132ed47c3031939320a33
Alexandr <IamWebDev@gmail.com>
2025-07-30 01:16:36 -0400

    Ask Bobo

--- src/components/BigHeader.tsx
+++ src/components/BigHeader.tsx
@@ -8,7 +8,7 @@ function BigHeader() {
                 <img src={logo} alt="Logo" className="w-36 h-36 object-contain" />
             </div>
             <div className="flex-grow pl-4">
-                <h1 className="text-5xl font-bold">AskBobo</h1>
+                <h1 className="text-5xl font-bold">Ask Bobo</h1>
                 <h1 className="text-1xl font-bold text-blue-400">The one and only Bukharian online dictionary.</h1>
             </div>
         </div>
